3bbf5f67161e2893a4b1bf1ed166cc73ba6ce329
DriveAway500 <155258716+DriveAway500@users.noreply.github.com>
2026-08-30 02:41:02 -0300

    bot instance refactored to be as on oficial lib examples

--- src/bot.py
+++ src/bot.py
@@ -1,38 +1,39 @@
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="__ENCHIRIDION_UNUSED_PREFIX_9f3a7c__", intents=intents)
+class EnchiridionBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix="__ENCHIRIDION_UNUSED_PREFIX_9f3a7c__",
+            intents=intents
+        )
 
-@bot.event
-async def setup_hook():
-    try:
-        #await bot.load_extension("cogs.ping_cog")
-        await bot.load_extension("cogs.exploit_cog")
-        await bot.load_extension("cogs.cve_cog")
-        await bot.load_extension("cogs.zeroday_cog")
-        await bot.load_extension("cogs.help_cog")
+    async def setup_hook(self):
+        try:
+            # await self.load_extension("cogs.ping_cog")
+            await self.load_extension("cogs.exploit_cog")
+            await self.load_extension("cogs.cve_cog")
+            await self.load_extension("cogs.zeroday_cog")
+            await self.load_extension("cogs.help_cog")
 
-    except Exception as e:
-        print(f"SETUP HOOK ERROR: {e}")
+            synced = await self.tree.sync()
+            print(f"SYNCHRONIZED {len(synced)} GLOBAL TREE COMMANDS.")
+        except Exception as e:
+            print(f"SETUP HOOK ERROR: {e}")
 
-@bot.event
-async def on_ready():
-    try:
-        synced = await bot.tree.sync()
-
-        activity = discord.CustomActivity(name="Procurando Exploits...")
-        await bot.change_presence(status=discord.Status.online, activity=activity)
-
-        print(f"SYNCHRONIZED {len(synced)} GLOBAL TREE COMMANDS.")
-        print(f"BOT CONNECTED AS: {bot.user}")
-
-    except Exception as e:
-        print(f"ON READY ERROR: {e}")
+    async def on_ready(self):
+        try:
+            activity = discord.CustomActivity(name="Procurando Exploits...")
+            await self.change_presence(status=discord.Status.online, activity=activity)
+            print(f"BOT CONNECTED AS: {self.user}")
+        except Exception as e:
+            print(f"ON READY ERROR: {e}")
 
 
 def enchiridion_run(token):
     try:
+        bot = EnchiridionBot()
         bot.run(token)
     except Exception as e:
         print(f"ERROR WHILE TRYING TO RUN: {e}")
