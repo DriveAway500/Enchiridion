be520543cd008815485dd29e3a3187c71ce4b8c0
DriveAway500 <155258716+DriveAway500@users.noreply.github.com>
2026-08-31 09:10:16 -0300

    changing file names to more precise ones

--- src/database/language_db.py
+++ src/database/language_db.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 import aiosqlite
 
 DEFAULT_DB_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "guild_settings.db"
+    os.path.dirname(os.path.abspath(__file__)), "language.db"
 )
 
 
