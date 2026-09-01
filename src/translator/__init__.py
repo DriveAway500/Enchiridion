98dfcb63e630797029552eb6fdef942f456135ea
DriveAway500 <155258716+DriveAway500@users.noreply.github.com>
2026-09-01 11:33:07 -0300

    refactored exploitdb command translation method

--- src/translator/__init__.py
+++ src/translator/__init__.py
@@ -2,3 +2,4 @@ from .engine import load_command_translation
 from .feedcog_t import feedcog_translate
 from .helpcog_t import helpcog_translate
 from .langcog_t import langcog_translate
+from .exploitcog_t import exploitdbcog_translate
