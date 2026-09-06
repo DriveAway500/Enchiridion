40e9ffcf093db6bbe7935c1b3a458795c244b2de
DriveAway500 <155258716+DriveAway500@users.noreply.github.com>
2026-09-06 19:32:23 -0300

    fixing cogs, removing test function (#4)

--- src/bot.py
+++ src/bot.py
@@ -5,13 +5,13 @@ from discord.ext import commands
 from feeds import setup_feed_task
 
 EXTENSIONS = (
-    # "cogs.exploit_cog",
+    "cogs.exploit_cog",
     "cogs.cve_cog",
-    # "cogs.zeroday_cog",
+    "cogs.zeroday_cog",
     "cogs.help_cog",
-    # "cogs.feed_cog",
-    # "cogs.lang_cog",
-    "cogs.test_cog"
+    "cogs.feed_cog",
+    "cogs.lang_cog",
+    #"cogs.test_cog"
 )
 
 class EnchiridionBot(commands.Bot):
